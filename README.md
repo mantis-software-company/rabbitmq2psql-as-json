f0a9573132d65868dce19903d2e56f84c0e602c4
Furkan Kalkan <furkankalkan@mantis.com.tr>
2021-05-11 17:32:48 +0300

    Codes added

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# rabbitmq2psql-as-json
+#rabbitmq2psql-as-json
 
+rabbitmq2psql-as-json is ready to use, basic asynchronous RabbitMQ consumer job library for PostgreSQL. It stops when queue is empty, so it can be useful for cron jobs, unit tests, CI/CD environments and production environments has slow datastream.
+
+## Usage
+```py
+import os
+import asyncio
+from rabbitmq2psql-as-json import consume
+
+if __name__ == '__main__':  
+    config = {  
+      "mq_host": os.environ.get('MQ_HOST'),  
+	  "mq_port": int(os.environ.get('MQ_PORT')),  
+	  "mq_vhost": os.environ.get('MQ_VHOST'),  
+	  "mq_user": os.environ.get('MQ_USER'),  
+	  "mq_pass": os.environ.get('MQ_PASS'),  
+	  "mq_queue": os.environ.get('MQ_QUEUE'),  
+	  "db_host": os.environ.get('DB_HOST'),  
+	  "db_port": int(os.environ.get('DB_PORT')),  
+	  "db_user": os.environ.get('DB_USER'),  
+	  "db_pass": os.environ.get('DB_PASS'),  
+	  "db_database": os.environ.get('DB_DATABASE')  
+    }  
+  
+sql_template = """insert into logs (body) values (%s);"""  
+  
+loop = asyncio.get_event_loop()  
+loop.run_until_complete(
+	consume(loop=loop, consumer_pool_size=10, sql_template=sql_template, config=config)
+)  
+loop.close()
+```
---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+setuptools.setup(
+    name="rabbitmq2psql-as-json",
+    version="1.0.0",
+    author="Furkan Kalkan",
+    author_email="furkankalkan@mantis.com.tr",
+    description="Asynchronous RabbitMQ consumer job library for PostgreSQL",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    platforms="all",
+    url="https://github.com/mantis-software-company/async-cj",
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Internet",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+	"Topic :: Software Development :: Testing",
+        "Intended Audience :: Developers",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8"
+    ],
+    install_requires=['psycopg2', 'aio_pika'],
+    python_requires=">3.5.*, <4",
+    packages=['rabbitmq2psql-as-json']
+)
